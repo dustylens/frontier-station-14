ec05e6268a7224c99472768169f1ce7ad392392b
dustylens <truecoffee@gmail.com>
2026-02-20 22:16:42 -0800

    into the pot

--- Content.Shared/MedicalScanner/HealthAnalyzerScannedUserMessage.cs
+++ Content.Shared/MedicalScanner/HealthAnalyzerScannedUserMessage.cs
@@ -3,10 +3,24 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.MedicalScanner;
 
 /// <summary>
-///     On interacting with an entity retrieves the entity UID for use with getting the current damage of the mob.
+/// On interacting with an entity retrieves the entity UID for use with getting the current damage of the mob.
 /// </summary>
 [Serializable, NetSerializable]
 public sealed class HealthAnalyzerScannedUserMessage : BoundUserInterfaceMessage
+{
+    public HealthAnalyzerUiState State;
+
+    public HealthAnalyzerScannedUserMessage(HealthAnalyzerUiState state)
+    {
+        State = state;
+    }
+}
+
+/// <summary>
+/// Contains the current state of a health analyzer control. Used for the health analyzer and cryo pod.
+/// </summary>
+[Serializable, NetSerializable]
+public struct HealthAnalyzerUiState
 {
     public readonly NetEntity? TargetEntity;
     public float Temperature;
@@ -14,10 +28,10 @@ public sealed class HealthAnalyzerScannedUserMessage : BoundUserInterfaceMessage
     public bool? ScanMode;
     public bool? Bleeding;
     public bool? Unrevivable;
-    public bool? Unclonable; // Frontier
-    public bool Printable; // Frontier
 
-    public HealthAnalyzerScannedUserMessage(NetEntity? targetEntity, float temperature, float bloodLevel, bool? scanMode, bool? bleeding, bool? unrevivable, bool? unclonable, bool printable = false) // Frontier: added unclonable, printable
+    public HealthAnalyzerUiState() {}
+
+    public HealthAnalyzerUiState(NetEntity? targetEntity, float temperature, float bloodLevel, bool? scanMode, bool? bleeding, bool? unrevivable)
     {
         TargetEntity = targetEntity;
         Temperature = temperature;
@@ -25,8 +39,5 @@ public sealed class HealthAnalyzerScannedUserMessage : BoundUserInterfaceMessage
         ScanMode = scanMode;
         Bleeding = bleeding;
         Unrevivable = unrevivable;
-        Unclonable = unclonable; // Frontier
-        Printable = printable; // Frontier
     }
 }
-
